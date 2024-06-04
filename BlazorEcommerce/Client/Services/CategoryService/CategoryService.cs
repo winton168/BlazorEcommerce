0047acbd18e0fe02e164be25458b0ddeb80489ba
Winton Wang <winton168@hotmail.com>
2024-06-04 00:26:02 -0400

    changes

---
+++ BlazorEcommerce/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace BlazorEcommerce.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly HttpClient _http;
+        public CategoryService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Category> Categories { get; set; }
+
+        public async Task GetCategories()
+        {
+            var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category");
+
+            if ( response != null && response.Data != null)
+            {
+                Categories = response.Data;
+            }
+        }
+    }
+}
---
+++ BlazorEcommerce/Client/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorEcommerce.Client.Services.CategoryService
+{
+    public interface ICategoryService
+    {
+        List<Category> Categories { get; }
+
+        Task GetCategories();
+
+    }
+}
