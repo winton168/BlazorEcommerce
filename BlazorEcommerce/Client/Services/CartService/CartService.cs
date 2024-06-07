b5bbe1a79b0916ee0c38d5d107c9be7abddd4b18
Winton Wang <winton168@hotmail.com>
2024-06-07 17:16:02 -0400

    end of 84

--- BlazorEcommerce/Client/Services/CartService/CartService.cs
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -1,4 +1,5 @@
 ﻿
+using BlazorEcommerce.Shared;
 using Blazored.LocalStorage;
 
 namespace BlazorEcommerce.Client.Services.CartService
@@ -6,10 +7,12 @@ namespace BlazorEcommerce.Client.Services.CartService
     public class CartService : ICartService
     {
         private readonly ILocalStorageService _localStorage;
+        private readonly HttpClient _http;
 
-        public CartService(ILocalStorageService localStorage)
+        public CartService(ILocalStorageService localStorage, HttpClient http)
         {
             _localStorage = localStorage;
+            _http = http;
         }
 
         public event Action OnChange;
@@ -23,9 +26,21 @@ namespace BlazorEcommerce.Client.Services.CartService
                 {
                     cart = new List<CartItem>();
                 }
-                cart.Add(cartItem);
+                
+                var sameItem = cart.Find( x => x.ProductId == cartItem.ProductId  && x.ProductTypeId == cartItem.ProductTypeId );
+                if (sameItem == null)
+                {
+                    cart.Add(cartItem);
+                }
+                else
+                {
+                    sameItem.Quantity += cartItem.Quantity;
+                }
 
+             
                 await _localStorage.SetItemAsync("cart", cart);
+
+                OnChange.Invoke();
             }
             catch(Exception ex)
             {
@@ -45,5 +60,55 @@ namespace BlazorEcommerce.Client.Services.CartService
 
             return cart;
         }
+
+        public async Task<List<CartProductResponse>> GetCartProducts()
+        {
+            var cartItems = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            var response = await _http.PostAsJsonAsync("api/cart/products", cartItems);
+            var cartProducts =
+                await response.Content.ReadFromJsonAsync<ServiceResponse<List<CartProductResponse>>>();
+
+            return cartProducts.Data;
+
+        }
+
+        public async Task RemoveProductFromCart(int productId, int productTypeId)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+
+            if ( cart == null)
+            {
+                return;
+            }
+
+            var cartItem = cart.Find( x => x.ProductId == productId && x.ProductTypeId == productTypeId );
+
+            if ( cartItem != null)
+            {
+                cart.Remove( cartItem );
+                await _localStorage.SetItemAsync("cart", cart);
+                OnChange.Invoke();
+            }
+
+        }
+
+        public async Task UpdateQuantity(CartProductResponse product)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+
+            if (cart == null)
+            {
+                return;
+            }
+            var cartItem = cart.Find(x => x.ProductId == product.ProductId && x.ProductTypeId == product.ProductTypeId);
+
+            if (cartItem != null)
+            {
+                cart.Remove(cartItem);
+                await _localStorage.SetItemAsync("cart", cart);
+                // OnChange.Invoke();
+            }
+        }
     }
+
 }
---
+++ BlazorEcommerce/Server/Services/CartService/ICartService.cs
@@ -0,0 +1,9 @@
+﻿using BlazorEcommerce.Client.Shared;
+
+namespace BlazorEcommerce.Server.Services.CartService
+{
+    public interface ICartService
+    {
+        Task<ServiceResponse<List<CartProductResponse>>> GetCartProducts(List<CartItem> cartItems); 
+    }
+}
