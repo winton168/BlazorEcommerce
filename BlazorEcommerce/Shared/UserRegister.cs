b283b2bf35bbdf64172a464feddc8cf961fb6297
Winton Wang <winton168@hotmail.com>
2024-06-13 22:20:51 -0400

    save changes

---
+++ BlazorEcommerce/Shared/UserRegister.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BlazorEcommerce.Shared
+{
+    public class UserRegister
+    {
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+}
